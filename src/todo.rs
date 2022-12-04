0d7cde3816dd08c09105502bbb37c14650108d86
Abhemanyu Sarin <abhemanyus@gmail.com>
2022-12-04 13:25:42 +0530

    update leptos-axum to fix issue

--- src/todo.rs
+++ src/todo.rs
@@ -8,7 +8,7 @@ cfg_if! {
         use sqlx::{Connection, SqliteConnection};
 
         pub async fn db() -> Result<SqliteConnection, ServerFnError> {
-            Ok(SqliteConnection::connect("sqlite:Todos.db").await.map_err(|e| ServerFnError::ServerError(e.to_string()))?)
+            Ok(SqliteConnection::connect("sqlite:Test.db").await.map_err(|e| ServerFnError::ServerError(e.to_string()))?)
         }
 
         pub fn register_server_functions() {
